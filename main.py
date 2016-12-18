55f1b6c0e9331ef51f74a63d80bcbb8aa04660dd
wanghe <wanghe.tw@gmail.com>
2016-12-18 16:02:17 +1300

    Add Save Button

--- main.py
+++ main.py
@@ -5,23 +5,28 @@ from kivy.utils import get_color_from_hex
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 
+import time
+
 # Color the background
 Window.clearcolor = get_color_from_hex("#f4f4f4")
 
-class CustomPopup(Popup):
+class AddNewPopup(Popup):
     pass
 
 class AppRootLayout(BoxLayout):
 
     # Popup
-    def open_popup(self):
-        the_popup = CustomPopup()
+    def open_addnew_popup(self):
+        the_popup = AddNewPopup()
         the_popup.open()
 
 class ShopListApp(App):
 
     def build(self):
         return AppRootLayout()
+
+    def getDate(self):
+        return (time.strftime("%Y-%m-%d", time.localtime()))
 
 if __name__ == '__main__':
     ShopListApp().run()
